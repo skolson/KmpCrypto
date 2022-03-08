5092ed0a2d2ee6335d566aed3a6d4ad88710ad14
skolson5903 <skolson5903@gmail.com>
2022-03-08 15:51:04 -0800

    Update android gradle plugin to AS Chipmunk beta version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        classpath("com.android.tools.build:gradle:7.1.2")
+        classpath("com.android.tools.build:gradle:7.2.0-beta03")
         classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.17.1")
     }
 }
